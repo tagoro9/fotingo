c21488490f4541d70ff49c88d1f58957cd10ad3c
Victor <tagoro9@gmail.com>
2017-03-15 19:10:51 -0700

    feat(Jira): improve issue transition setup (#24)
    * Use a set of regex to identify transition ids.
    * Fallback to question if can't find any of the ids.
    * Improve UI test so users can understand what is being asked better.
    
    Closes #11.

--- src/issue-tracker/jira/status.js
+++ src/issue-tracker/jira/status.js
@@ -9,7 +9,20 @@ export const status = {
   SELECTED_FOR_DEVELOPMENT: 'SELECTED_FOR_DEVELOPMENT'
 };
 
+const statusRegex = {
+  BACKLOG: /backlog/i,
+  IN_PROGRESS: /in progress/i,
+  IN_REVIEW: /review|done/i,
+  SELECTED_FOR_DEVELOPMENT: /(todo)|(to do)|(selected for development)/i
+};
+
+const statusMatcher = statusToFind => R.compose(
+  R.ifElse(R.isNil, R.identity, R.compose(parseInt, R.prop('id'))),
+  R.find(R.compose(R.test(statusRegex[statusToFind]), R.prop('name')))
+);
+
 export default R.curryN(2, (config, issue) => {
+
   const askForStatus = R.composeP(
     config.update(['jira', 'status']),
     R.compose(
@@ -23,12 +36,44 @@ export default R.curryN(2, (config, issue) => {
     R.compose(
       reporter.question,
       R.always({
-        question: 'What are the ids for the transitions that represent\n' +
-        'Backlog, to do, in progress, in review (enter a comma separated list)?'
+        question: 'Please, insert a comma separated list of the ids'
       }),
       reporter.info,
-      R.concat('These are the possible transitions found for the issue:\n'),
+      R.concat(R.__,
+        '\n     We need you to input the ids of the \'Backlog\', \'To do\', \'In progress\', \'In review\'\n    ' +
+        ' (one state can represent multiple values (e.g. \'Backlog\' and \'To do\' could be the same id).\n     ' +
+        'It all depends on your workflow.\n'
+      ),
+      R.concat(
+        'In order to update the jira issues correctly, we need to know a little bit more about your workflow.\n     ' +
+        'We need to identify the different states an issue can be on. We tried inferring those values, but we\n     ' +
+        'were unable to do so. We are looking for 4 specific states: \'Backlog\', \'To do\', \'In progress\'\n     ' +
+        'and \'In review\'. These are the names and ids of the states the issue can transition to:\n     '
+      ),
     )
+  );
+
+  const inferStatus = R.compose(
+    R.ifElse(
+      R.compose(R.any(R.isNil), R.values),
+      R.compose(
+        askForStatus,
+        R.concat(R.__, '\n'),
+        R.concat('\n'),
+        R.join('\n'),
+        R.map(({ name, id }) => `         * ${name}: ${id}`),
+        R.prop('transitions'),
+      ),
+      R.compose(wrapInPromise, config.update(['jira', 'status']), R.omit(['transitions'])),
+    ),
+    transitions => ({
+      [status.BACKLOG]:
+        statusMatcher(status.BACKLOG)(transitions) || statusMatcher(status.SELECTED_FOR_DEVELOPMENT)(transitions),
+      [status.IN_PROGRESS]: statusMatcher(status.IN_PROGRESS)(transitions),
+      [status.IN_REVIEW]: statusMatcher(status.IN_REVIEW)(transitions),
+      [status.SELECTED_FOR_DEVELOPMENT]: statusMatcher(status.SELECTED_FOR_DEVELOPMENT)(transitions),
+      transitions
+    })
   );
 
   return R.ifElse(
@@ -36,9 +81,7 @@ export default R.curryN(2, (config, issue) => {
       R.either(R.isNil, R.isEmpty), R.invoker(1, 'get')(['jira', 'status'])
     ),
     R.partial(R.compose(
-      askForStatus,
-      R.join('\n'),
-      R.map(({ name, id }) => ` * ${name}: ${id}`),
+      inferStatus,
       R.map(R.prop('to')),
       R.prop('transitions')
     ), [issue]),
