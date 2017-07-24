a0f0fc37495a6c5b08b61f7dccb6800c74bc825e
Alan Pearce <alan@alanpearce.eu>
2017-07-24 17:48:55 +0200

    fix(git): extract information from branch name according to branch template (#51)
    If using a different branch template than the default one, then when calling `review` the issue information could not be found.

--- src/git/util.js
+++ src/git/util.js
@@ -8,6 +8,12 @@ export const ISSUE_TYPES = {
   Task: 'c',
 };
 
+const TEMPLATE_KEY_MATCHERS = {
+  'issue.key': '(\\w+-\\d+)',
+  'issue.shortName': '\\w+',
+  'issue.sanitizedSummary': '[\\w_-]+',
+};
+
 const sanitizeSummary = R.compose(
   R.take(72),
   R.replace(/(_|-)$/, ''),
@@ -21,9 +27,12 @@ const sanitizeSummary = R.compose(
 
 const defaultBranchTemplate = '{issue.shortName}/{issue.key}_{issue.sanitizedSummary}';
 
+const getTemplate = config => config.get(['jira', 'templates', 'branch']) || defaultBranchTemplate;
+
 const getTemplateData = (config, issue) => ({
-  template: config.get(['jira', 'templates', 'branch']) || defaultBranchTemplate,
-  data: {
+  template: getTemplate(config),
+  matchers: TEMPLATE_KEY_MATCHERS,
+  data: issue && {
     'issue.shortName': config.get(['jira', 'issueTypes', issue.fields.issuetype.id]).shortName,
     'issue.key': issue.key.toLowerCase(),
     'issue.sanitizedSummary': sanitizeSummary(issue.fields.summary),
@@ -44,8 +53,21 @@ export const createBranchName = R.curryN(
 
 export const getProject = R.compose(R.nth(1), R.match(/\/((\w|-)+)$/));
 
-export const getIssueIdFromBranch = R.compose(
-  R.ifElse(R.isNil, R.identity, R.toUpper),
-  R.last,
-  R.match(/\w\/(\w+-\d+)/),
+export const getIssueIdFromBranch = R.curryN(
+  2,
+  R.compose(
+    R.converge(R.compose(R.ifElse(R.isNil, R.identity, R.toUpper), R.last, R.match), [
+      R.compose(
+        // This returns the regex
+        R.reduce(
+          (msg, [k, v]) => R.replace(`{${k}}`, v, msg),
+          R.__, // Here goes the template
+          R.toPairs(TEMPLATE_KEY_MATCHERS),
+        ),
+        getTemplate, // New function that I suggested above
+        R.nthArg(0), // Config
+      ),
+      R.nthArg(1), // Branch name
+    ]),
+  ),
 );
