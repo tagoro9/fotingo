b9f546a9cc3bfd5b08fa37f6bbd49519210f1fe4
Victor <victor@turo.com>
2017-10-20 18:18:57 +0100

    feat(release): create release command

--- src/fotingo-release.js
+++ src/fotingo-release.js
@@ -1,25 +1,56 @@
 import program from 'commander';
 import R from 'ramda';
 
+import { getProject } from './git/util';
 import { handleError } from './error';
 import config from './config';
 import init from './init';
 import reporter from './reporter';
+import { wrapInPromise } from './util';
 
-// const getReleaseName = R.compose(R.head, R.prop('args'));
+const getReleaseId = R.compose(R.head, R.prop('args'));
 
 try {
   program
     .option('-n, --no-branch-issue', 'Do not pick issue from the branch name')
-    .option('-s, --simple', 'Do not use any issue tracker')
     .option('-i, --issue [issue]', 'Specify more issues to include in the release', R.append, [])
     .parse(process.argv);
 
-  const { step } = reporter.stepFactory(1);
+  const releaseId = getReleaseId(program);
+  const shouldGetIssue = R.compose(R.equals(true), R.prop('branchIssue'))(program);
+  const project = getProject(process.cwd());
+
+  const { step, stepCurried, stepCurriedP } = reporter.stepFactory(5);
   step(1, 'Initializing services', 'rocket');
-  init(config, program)
-    .then(R.partial(reporter.footer, [null]))
-    .catch(handleError);
+  R.composeP(
+    reporter.footer,
+    ({ git, github, issueTracker }) =>
+      R.composeP(
+        ({ issues, notes }) =>
+          R.composeP(
+            github.createOrUpdateRelease(config, project, notes, releaseId),
+            stepCurriedP(5, `Creating release ${releaseId} in github`, 'ship'),
+            issueTracker.setIssuesFixVersion(issues),
+            stepCurried(4, 'Setting the fix version to the affected issues', 'pencil2'),
+            issueTracker.createVersion(releaseId),
+          )(issues),
+        stepCurried(3, `Creating release ${releaseId} in ${issueTracker.name}`, 'ship'),
+        R.apply(R.merge),
+        R.converge((...promises) => Promise.all(promises), [
+          R.compose(wrapInPromise, R.set(R.lensProp('issues'), R.__, {})),
+          R.composeP(
+            R.set(R.lensProp('notes'), R.__, {}),
+            issueTracker.createReleaseNotes(releaseId),
+          ),
+        ]),
+        promises => Promise.all(promises),
+        R.map(issueTracker.getIssue),
+        issues =>
+          stepCurried(2, `Getting ${issues.join(', ')} from ${issueTracker.name}`, 'bug', issues),
+        git.getIssuesInBranch(config, program.issue, shouldGetIssue),
+      )(),
+    init,
+  )(config, program).catch(handleError);
 } catch (e) {
   handleError(e);
   program.help();
