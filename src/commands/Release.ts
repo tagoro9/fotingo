64b78e077c5821e9697f21a6e5919b83b798df7d
Victor <victor@turo.com>
2019-12-05 19:50:33 -0800

    feat(Release): include jira release on the notes

--- src/commands/Release.ts
+++ src/commands/Release.ts
@@ -7,6 +7,7 @@ import { Emoji, Messenger } from 'src/io/messenger';
 import { CreateRelease } from 'src/issue-tracker/Issue';
 import { Jira } from 'src/issue-tracker/Jira';
 import { Tracker } from 'src/issue-tracker/Tracker';
+import { getReleaseNotes } from 'src/templates/getReleaseNotes';
 
 import { FotingoArguments } from './FotingoArguments';
 import { getLocalChangesInformation, LocalChanges } from './util';
@@ -40,22 +41,34 @@ const getCommandData = (args: FotingoArguments): ReleaseData => {
   };
 };
 
-// TODO Fix return type
-export const cmd = (args: FotingoArguments, messenger: Messenger): Observable<any> => {
+export const cmd = (args: FotingoArguments, messenger: Messenger): Observable<JointRelease> => {
   const git: Git = new Git(args.config.git, messenger);
   const jira: Tracker = new Jira(args.config.jira, messenger);
   const github: Remote = new Github(args.config.github, messenger, git);
   const commandData$ = of(args).pipe(map(getCommandData));
-  return commandData$.pipe(
+
+  const releaseInformation$ = commandData$.pipe(
     switchMap(git.getBranchInfo),
     withLatestFrom(commandData$),
     switchMap(getLocalChangesInformation(jira, messenger)),
     withLatestFrom(commandData$),
     map(buildReleaseData),
+  );
+
+  return releaseInformation$.pipe(
     tap(data => messenger.emit(`Creating release ${data.name}`, Emoji.SHIP)),
     switchMap(jira.createRelease),
     switchMap(jira.setIssuesFixVersion),
-    switchMap(github.createRelease),
+    withLatestFrom(commandData$),
+    switchMap(([release, { useDefaults }]) =>
+      getReleaseNotes(args.config.release, messenger, release, useDefaults).pipe(
+        map(notes => ({
+          notes,
+          release,
+        })),
+      ),
+    ),
+    switchMap(({ notes, release }) => github.createRelease(release, notes)),
     tap((data: JointRelease) =>
       messenger.emit(
         `Release created: ${data.release.url} | ${data.remoteRelease.url}`,
