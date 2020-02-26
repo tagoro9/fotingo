5a16d4a8a38b81fbdda3c6252cf35b54ea8c5185
Victor <victor@turo.com>
2020-02-25 22:24:21 -0800

    feat(SelectRequest): add support to search within the options

--- src/ui/SelectRequest.tsx
+++ src/ui/SelectRequest.tsx
@@ -1,19 +1,61 @@
 import React = require('react');
 
-import { Box } from 'ink';
-import SelectInput, { Item } from 'ink-select-input';
+import { Box, useInput } from 'ink';
+import InkTextInput from 'ink-text-input';
+import { useState } from 'react';
 
+import { ControlledSelectInput } from './ControlledSelectInput';
 import { SelectRequestProps } from './props';
 import { Question } from './Question';
 
+/**
+ * Component that given a select request, will display the options allowing the user to select
+ * the one they want. It allows users to search among the options if the allowTextSearch option is set
+ */
 export function SelectRequest({ onSubmit, request }: SelectRequestProps): JSX.Element {
+  const useTextSearch = request.allowTextSearch === true;
+  const [searchText, setSearchText] = useState('');
+  const [items, setItems] = useState(request.options);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  useInput((_, key) => {
+    console.error('There was an input');
+    if (key.downArrow) {
+      setSelectedIndex((selectedIndex + 1) % items.length);
+    }
+    if (key.return) {
+      onSubmit(items[selectedIndex].value);
+    }
+    if (key.upArrow) {
+      setSelectedIndex((((selectedIndex - 1) % items.length) + items.length) % items.length);
+    }
+  });
+
+  const onInputChange = (text: string): void => {
+    const escapedText = text.replace(/\\u001b/g, '');
+    setSearchText(escapedText);
+    if (escapedText && escapedText.trim() !== '') {
+      setItems(
+        request.options.filter(option =>
+          option.label.toLowerCase().includes(escapedText.toLowerCase()),
+        ),
+      );
+    } else {
+      setItems(request.options);
+    }
+    setSelectedIndex(0);
+  };
+
   return (
     <Box flexDirection="column">
-      <Question {...request} />
-      <SelectInput
-        items={request.options}
-        onSelect={(item: Item): void => void onSubmit(String(item.value))}
-      />
+      <Box>
+        <Question {...request} />
+        {useTextSearch ? (
+          <InkTextInput showCursor={true} value={searchText} onChange={onInputChange} />
+        ) : null}
+      </Box>
+      <Box marginBottom={1} marginLeft={1} marginTop={1}>
+        <ControlledSelectInput items={items} selectedIndex={selectedIndex} />
+      </Box>
     </Box>
   );
 }
---
+++ test/ui/SelectRequest.test.tsx
@@ -0,0 +1,47 @@
+import 'jest';
+
+import { render } from 'ink-testing-library';
+import { MessageType, RequestType, SelectRequest as SelectRequestType } from 'src/io/messenger';
+import { SelectRequest } from 'src/ui/SelectRequest';
+
+import React = require('react');
+
+describe('<SelectRequest />', () => {
+  const request: SelectRequestType = {
+    allowTextSearch: true,
+    message: 'Select an option',
+    options: [
+      { label: 'Item 1', value: 1 },
+      { label: 'Item 2', value: 2 },
+      { label: 'Item 3', value: 3 },
+    ],
+    showSpinner: false,
+    requestType: RequestType.SELECT,
+    type: MessageType.REQUEST,
+  };
+
+  test('displays the list of options filtering by the text input', async () => {
+    const actual = render(<SelectRequest onSubmit={jest.fn()} request={request} />);
+    expect(actual.lastFrame()).toMatchSnapshot();
+    actual.stdin.write('1');
+    expect(actual.lastFrame()).toMatchSnapshot();
+  });
+
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('allows to navigate the list of options using the arrow keys', async () => {
+    const ARROW_DOWN = '\u001B[B';
+
+    const actual = render(
+      <SelectRequest
+        onSubmit={jest.fn()}
+        request={{
+          ...request,
+          allowTextSearch: false,
+        }}
+      />,
+    );
+    expect(actual.lastFrame()).toMatchSnapshot();
+    actual.stdin.write(ARROW_DOWN);
+    expect(actual.lastFrame()).toMatchSnapshot();
+  });
+});
