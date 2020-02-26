3dcff6c2fdeeaf2ef5bb73e128b93a1ebbc59db8
Victor <victor@turo.com>
2020-02-25 16:52:29 -0800

    feat(ui): create a ControlledSelectInput component

---
+++ test/ui/ControlledSelectInput.test.tsx
@@ -0,0 +1,26 @@
+import 'jest';
+import React = require('react');
+
+import { render } from 'ink-testing-library';
+import { ControlledSelectInput } from 'src/ui/ControlledSelectInput';
+
+describe('<ControlledSelectInput />', () => {
+  const items = [
+    { label: 'Item 1', value: 1 },
+    { label: 'Item 2', value: 2 },
+  ];
+
+  test('renders the list of items', () => {
+    const props = {
+      items,
+    };
+    const { lastFrame } = render(<ControlledSelectInput {...props} />);
+    expect(lastFrame()).toMatchSnapshot();
+  });
+
+  test('renders the selected item with a pointer', () => {
+    const props = { items, selectedIndex: 1 };
+    const { lastFrame } = render(<ControlledSelectInput {...props} />);
+    expect(lastFrame()).toMatchSnapshot();
+  });
+});
