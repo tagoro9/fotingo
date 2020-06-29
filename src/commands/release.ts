b8957e3fe33c0770b56338e3e6af81f551e51460
Victor <victor@turo.com>
2020-06-29 07:49:16 -0700

    refactor(commands): use oclif and FotingoCommand

---
+++ src/commands/release.ts
@@ -0,0 +1,94 @@
+import { flags } from '@oclif/command';
+import { Observable } from 'rxjs';
+import { map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { yes } from 'src/cli/flags';
+import { FotingoCommand } from 'src/cli/FotingoCommand';
+import { Emoji } from 'src/io/messenger';
+import { getReleaseNotes } from 'src/templates/getReleaseNotes';
+import { CreateRelease, JointRelease, LocalChanges, ReleaseData } from 'src/types';
+
+export class Release extends FotingoCommand<JointRelease, ReleaseData> {
+  static description = 'Create a release with your changes';
+
+  static args = [
+    {
+      description: 'Name of the release to be created',
+      name: 'release-name',
+      required: true,
+    },
+  ];
+
+  static flags = {
+    issue: flags.string({
+      char: 'i',
+      description: 'Specify more issues to include in the release',
+      multiple: true,
+      required: false,
+    }),
+    simple: flags.boolean({
+      char: 's',
+      description: 'Do not use any issue tracker',
+      name: 'simple',
+    }),
+    yes,
+  };
+
+  /**
+   * Given the local changes and a release data, build the object needed
+   * to create a release in the issue tracker
+   */
+  private static buildReleaseData([data, { tracker, ...releaseData }]: [
+    LocalChanges,
+    ReleaseData,
+  ]): CreateRelease {
+    return {
+      ...releaseData,
+      ...data,
+      submitRelease: tracker.enabled,
+    };
+  }
+
+  protected getCommandData(): ReleaseData {
+    const { args, flags } = this.parse(Release);
+    return {
+      issues: (flags.issue || []) as string[],
+      name: args.releaseName as string,
+      tracker: {
+        enabled: !flags.simple,
+      },
+      useDefaults: flags.yes as boolean,
+    };
+  }
+
+  protected runCmd(commandData$: Observable<ReleaseData>): Observable<JointRelease> {
+    const releaseInformation$ = commandData$.pipe(
+      switchMap(this.git.getBranchInfo),
+      withLatestFrom(commandData$),
+      switchMap(this.getLocalChangesInformation),
+      withLatestFrom(commandData$),
+      map(Release.buildReleaseData),
+    );
+
+    return releaseInformation$.pipe(
+      tap((data) => this.messenger.emit(`Creating release ${data.name}`, Emoji.SHIP)),
+      switchMap(this.tracker.createRelease),
+      switchMap(this.tracker.setIssuesFixVersion),
+      withLatestFrom(commandData$),
+      switchMap(([release, { useDefaults }]) =>
+        getReleaseNotes(this.fotingo.release, this.messenger, release, useDefaults).pipe(
+          map((notes) => ({
+            notes,
+            release,
+          })),
+        ),
+      ),
+      switchMap(({ notes, release }) => this.github.createRelease(release, notes)),
+      tap((data: JointRelease) =>
+        this.messenger.emit(
+          `Release created: ${data.release.url} | ${data.remoteRelease.url}`,
+          Emoji.LINK,
+        ),
+      ),
+    );
+  }
+}
---
+++ src/commands/start.ts
@@ -0,0 +1,146 @@
+import { flags } from '@oclif/command';
+import { compose, has, ifElse, pathEq, prop, tap as rTap, unapply, zipObj } from 'ramda';
+import { Observable, of } from 'rxjs';
+import { switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { branch } from 'src/cli/flags';
+import { FotingoCommand } from 'src/cli/FotingoCommand';
+import { Emoji } from 'src/io/messenger';
+import { Issue, IssueStatus, IssueType, StartData } from 'src/types';
+
+interface IssueAndStartData {
+  commandData: StartData;
+  issue: Issue;
+}
+
+export class Start extends FotingoCommand<Issue | void, StartData> {
+  static description = 'Start working on an issue';
+
+  static args = [
+    {
+      name: 'issue',
+      required: true,
+      description: 'Id / Title of the issue to start working with',
+    },
+  ];
+
+  static flags = {
+    branch,
+    'no-branch-issue': flags.boolean({
+      char: 'a',
+      default: false,
+      description: 'Do not create a branch with the issue name',
+      name: 'no-branch-issue',
+    }),
+    create: flags.boolean({
+      char: 'c',
+      default: false,
+      description: 'Create a new issue instead of searching for it',
+      required: false,
+      dependsOn: ['project', 'type'],
+    }),
+    project: flags.string({
+      char: 'p',
+      description: 'Name of the project where to create the issue',
+      required: false,
+      dependsOn: ['type', 'create'],
+    }),
+    type: flags.string({
+      char: 't',
+      description: 'Type of issue to be created',
+      required: false,
+      dependsOn: ['create', 'project'],
+    }),
+    description: flags.string({
+      char: 'd',
+      description: 'Description of the issue to be created',
+      required: false,
+      dependsOn: ['create', 'project', 'type'],
+    }),
+    labels: flags.string({
+      description: 'Labels to add to the issue',
+      char: 'l',
+      multiple: true,
+      required: false,
+      dependsOn: ['create', 'project', 'type'],
+    }),
+  };
+
+  getCommandData(): StartData {
+    const { args, flags } = this.parse(Start);
+    return {
+      git: {
+        createBranch: !flags['no-branch-issue'],
+      },
+      issue: args.create
+        ? {
+            description: flags.description,
+            labels: flags.labels,
+            project: flags.project!,
+            title: args.issue,
+            type: flags.type as IssueType,
+          }
+        : { id: args.issue },
+    };
+  }
+
+  /**
+   * Get or create an issue in the tracker based on the passed arguments
+   */
+  getOrCreateIssue = compose(
+    ifElse(
+      has('id'),
+      compose(
+        this.tracker.getIssue,
+        rTap((id) => {
+          this.messenger.emit(`Getting ${id} from ${this.tracker.name}`, Emoji.BUG);
+        }),
+        prop('id'),
+      ),
+      compose(
+        this.tracker.createIssueForCurrentUser,
+        rTap(() => {
+          this.messenger.emit(`Creating issue in ${this.tracker.name}`, Emoji.BUG);
+        }),
+      ),
+    ),
+    prop('issue'),
+  );
+
+  /**
+   * Set an issue in progress
+   */
+  setIssueInProgress = compose(
+    (id: string) => this.tracker.setIssueStatus(IssueStatus.IN_PROGRESS, id),
+    prop('key'),
+  );
+
+  /**
+   * Create a branch for the specified issue stashing all the changes
+   */
+  createBranch = compose(
+    this.git.createBranchAndStashChanges,
+    rTap((name) => {
+      this.messenger.emit(`Creating branch ${name}`, Emoji.TADA);
+    }),
+    this.git.getBranchNameForIssue,
+    prop('issue'),
+  );
+
+  shouldCreateBranch = pathEq(['commandData', 'git', 'createBranch'], true);
+
+  justReturnTheIssue = compose((data: IssueAndStartData) => of(prop('issue', data)));
+
+  runCmd(commandData$: Observable<StartData>): Observable<Issue | void> {
+    return commandData$.pipe(
+      switchMap(this.getOrCreateIssue),
+      tap((issue: Issue) => {
+        this.messenger.emit(`Setting ${issue.key} in progress`, Emoji.BOOKMARK);
+      }),
+      switchMap(this.setIssueInProgress),
+      withLatestFrom(commandData$, unapply(zipObj(['issue', 'commandData']))),
+      switchMap<IssueAndStartData, Observable<void | Issue>>(
+        ifElse(this.shouldCreateBranch, compose(this.createBranch), this.justReturnTheIssue),
+      ),
+    );
+  }
+}
