f3738f4e01e4bfe958a652027db91de8c9b39ad5
Victor <victor@turo.com>
2022-12-23 22:21:20 +0000

    refactor(http-client): use axios instead of request

--- test/network/HttpClient.test.ts
+++ test/network/HttpClient.test.ts
@@ -1,67 +1,52 @@
-import { afterEach, beforeEach, describe, expect, jest, test } from '@jest/globals';
-import * as req from 'request';
+import { beforeEach, describe, expect, test } from '@jest/globals';
+import nock, { DataMatcherMap } from 'nock';
 import { lastValueFrom } from 'rxjs';
 import { HttpClient } from 'src/network/HttpClient';
 
-jest.mock('request');
-const mockRequest = req as unknown as ReturnType<typeof jest.fn>;
-
-const mockRequestWithSuccess = (body: unknown, statusCode = 200): void =>
-  void mockRequest.mockImplementation(
-    (_: unknown, callback: (a: unknown, b: unknown, c: unknown) => void) => {
-      callback(undefined, { statusCode, body }, body);
-    },
-  );
+const mockRequestWithSuccess = (
+  api: nock.Scope,
+  statusCode: number,
+  body: Record<string, unknown>,
+  query: DataMatcherMap | boolean = true,
+): void => {
+  api.get('/').query(query).reply(statusCode, body);
+};
 
 describe('HttpClient', () => {
   let client: HttpClient;
+  let api: nock.Scope;
+
   beforeEach(() => {
-    client = new HttpClient({ root: 'https://fotin.go' });
-  });
-  afterEach(() => {
-    mockRequest.mockReset();
+    const root = 'https://fotin.go';
+    api = nock(root);
+    client = new HttpClient({ root: root });
   });
 
   describe('get', () => {
     test('fetches JSON from the server', () => {
       const body = { key: 'value' };
-      mockRequestWithSuccess(body);
+      mockRequestWithSuccess(api, 200, body);
       return lastValueFrom(client.get('/')).then((value) => {
-        expect(value).toEqual({
-          body,
-          response: { statusCode: 200, body },
-        });
-        expect(mockRequest).toHaveBeenCalledTimes(1);
-        expect(mockRequest).toHaveBeenCalledWith(
-          {
-            headers: { accept: 'application/json' },
-            json: true,
-            method: 'get',
-            url: 'https://fotin.go/',
-          },
-          expect.any(Function),
-        );
+        expect(value.body).toEqual(body);
+        expect(api.isDone()).toBe(true);
       });
     });
 
     test('passes the query string to the server', () => {
-      mockRequestWithSuccess({});
+      mockRequestWithSuccess(api, 200, {}, { val: true });
       return lastValueFrom(client.get('/', { qs: { val: true } })).then(() => {
-        expect(mockRequest).toHaveBeenCalledWith(
-          expect.objectContaining({
-            qs: { val: true },
-          }),
-          expect.any(Function),
-        );
+        expect(api.isDone()).toBe(true);
       });
     });
 
     test('throws an error when the call fails', () => {
       const body = { error: 'message' };
-      mockRequestWithSuccess(body, 400);
+      mockRequestWithSuccess(api, 400, body);
       return lastValueFrom(client.get('/')).catch((error) => {
         // eslint-disable-next-line jest/no-conditional-expect
-        expect(error.body).toBe(body);
+        expect(api.isDone()).toBe(true);
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(error.body).toEqual(body);
       });
     });
   });
